89ddd76481a62cc7636cff0f8410151a4d6c0adb
Fabian Köhler <fabian.koehler@protonmail.ch>
2022-07-22 14:10:33 +0200

    Store magnitude for overlap.

--- mpskit/cmd/overlap.cpp
+++ mpskit/cmd/overlap.cpp
@@ -14,7 +14,7 @@ int cmdOverlap(const std::string &psi_path, const std::string &psi2_path, const 
     std::cout << "overlap = " << overlap_abs << '\n';
 
     H5Easy::File file(output_path, H5Easy::File::Overwrite | H5Easy::File::Create);
-    H5Easy::dump(file, "overlap", overlap_abs);
+    H5Easy::dump(file, "overlap", overlap_abs * overlap_abs);
 
     return 0;
 }
